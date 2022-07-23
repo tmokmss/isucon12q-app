44169eaecff7790fb21356d6c85fe88871eeb9ab
tmokmss <tomookam@live.jp>
2022-07-23 15:25:56 +0900

    fix

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -35,4 +35,4 @@ CREATE TABLE player_score (
 
 CREATE INDEX `comp_pla_row_idx` ON player_score (`competition_id`, `player_id`, `row_num`);
 CREATE INDEX `comp_row_idx` ON player_score (`competition_id`, `row_num`);
-CREATE INDEX `pla_idx` ON player_scor (`player_id`);
+CREATE INDEX `player_idx` ON player_score (`player_id`);
